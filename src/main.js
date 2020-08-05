148784dbd5763752fa9878b3ca4ff19b9c8aaa99
Vadim Kozin <ulin.salma@yandex.ru>
2020-08-05 16:06:50 +0300

    переписал модуль task-edit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import {createSiteMenuTemplate} from './view/site-menu';
 import {createFilterTemplate} from './view/filter';
-import {createBoardTemplate} from './view/board';
-import {createSortTemplate} from './view/sort';
 import {createTaskTemplate} from './view/task';
 import {createTaskEditTemplate} from './view/task-edit';
+import {createBoardTemplate} from './view/board';
+import {createSortTemplate} from './view/sort';
 import {createLoadMoreButtonTemplate} from './view/load-more-button';
 
-const TASK_COUNT = 3;
+import {generateTask} from "./mock/task.js";
+import {generateFilter} from "./mock/filter.js";
+
+const TASK_COUNT = 18;
+const TASK_COUNT_PER_STEP = 8;
+
+const tasks = new Array(TASK_COUNT).fill().map(generateTask);
+const filters = generateFilter(tasks);
 
 const Position = {
   AFTER_BEGIN: `afterbegin`,
@@ -28,10 +35,11 @@ const boardElement = siteMainElement.querySelector(`.board`);
 const taskListElement = boardElement.querySelector(`.board__tasks`);
 
 render(boardElement, createSortTemplate(), Position.AFTER_BEGIN);
-render(taskListElement, createTaskEditTemplate());
+render(taskListElement, createTaskEditTemplate(tasks[0]));
 
-for (let i = 0; i < TASK_COUNT; i++) {
-  render(taskListElement, createTaskTemplate());
+for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
+  render(taskListElement, createTaskTemplate(tasks[i]));
 }
+
 
 render(boardElement, createLoadMoreButtonTemplate());
