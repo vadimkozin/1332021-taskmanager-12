8bf37f9b00b2d29af25d299b159e960f16876853
Vadim Kozin <ulin.salma@yandex.ru>
2020-08-13 15:07:49 +0300

    добавил класс TaskList (task-list.js)

---
+++ src/view/task-list.js
@@ -0,0 +1,25 @@
+import {createElement} from "../utils.js";
+
+const createTaskListTemplate = () => `<div class="board__tasks"></div>`;
+
+export default class TaskList {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTaskListTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
