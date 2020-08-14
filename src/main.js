12d608608a3d342c3bb664b3bcccff7ad0a5bada
Vadim Kozin <ulin.salma@yandex.ru>
2020-08-14 16:43:58 +0300

    добавил сообщение если нет задач или они все в архиве

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import SiteMenuView from './view/site-menu.js';
 import FilterView from './view/filter';
 import TaskView from './view/task';
 import TaskEditView from './view/task-edit';
+import TaskListView from './view/task-list';
+import NoTaskView from './view/no-task';
 import LoadMoreButtonView from './view/load-more-button';
 import BoardView from './view/board';
 import SortView from './view/sort';
@@ -59,38 +61,43 @@ render(siteMainElement, new FilterView(filters).getElement());
 render(siteMainElement, new BoardView().getElement());
 
 const boardElement = siteMainElement.querySelector(`.board`);
-const taskListElement = boardElement.querySelector(`.board__tasks`);
+const taskListElement = new TaskListView().getElement();
 
-render(boardElement, new SortView().getElement(), RenderPosition.AFTER_BEGIN);
+if (tasks.every((task) => task.isArchive)) {
+  render(boardElement, new NoTaskView().getElement());
+} else {
+  render(boardElement, new SortView().getElement(), RenderPosition.AFTER_BEGIN);
+  render(boardElement, taskListElement);
 
-tasks
-  .slice(0, Math.min(tasks.length, Task.COUNT_PER_STEP))
-  .forEach((task) => renderTask(taskListElement, task));
+  tasks
+    .slice(0, Math.min(tasks.length, Task.COUNT_PER_STEP))
+    .forEach((task) => renderTask(taskListElement, task));
 
-if (tasks.length > Task.COUNT_PER_STEP) {
+  if (tasks.length > Task.COUNT_PER_STEP) {
 
-  let count = Task.COUNT_PER_STEP;
+    let count = Task.COUNT_PER_STEP;
 
-  const loadMoreElement = new LoadMoreButtonView().getElement();
+    const loadMoreElement = new LoadMoreButtonView().getElement();
 
-  render(boardElement, loadMoreElement);
+    render(boardElement, loadMoreElement);
 
-  const onClickLoadMoreButton = (evt) => {
-    evt.preventDefault();
+    const onClickLoadMoreButton = (evt) => {
+      evt.preventDefault();
 
-    tasks
-      .slice(count, count + Task.COUNT_PER_STEP)
-      .forEach((task) => renderTask(taskListElement, task));
+      tasks
+        .slice(count, count + Task.COUNT_PER_STEP)
+        .forEach((task) => renderTask(taskListElement, task));
 
-    count += Task.COUNT_PER_STEP;
+      count += Task.COUNT_PER_STEP;
 
-    if (count > tasks.length) {
-      loadMoreElement.removeEventListener(`click`, onClickLoadMoreButton);
-      loadMoreElement.remove();
-    }
+      if (count > tasks.length) {
+        loadMoreElement.removeEventListener(`click`, onClickLoadMoreButton);
+        loadMoreElement.remove();
+      }
 
-  };
+    };
 
-  loadMoreElement.addEventListener(`click`, onClickLoadMoreButton);
+    loadMoreElement.addEventListener(`click`, onClickLoadMoreButton);
 
+  }
 }
--- src/view/no-task.js
+++ src/view/no-task.js
@@ -1,19 +1,15 @@
 import {createElement} from "../utils.js";
 
-export const createBoardTemplate = () => `<section class="board container"></section>`;
+const createNoTaskTemplate = () =>
+  `<p class="board__no-tasks"> Click «ADD NEW TASK» in menu to create your first task </p>`;
 
-export const createBoardTemplate__ = () =>
-  `<section class="board container">
-    <div class="board__tasks"></div>
-  </section>`;
-
-export default class Board {
+export default class NoTask {
   constructor() {
     this._element = null;
   }
 
   getTemplate() {
-    return createBoardTemplate();
+    return createNoTaskTemplate();
   }
 
   getElement() {
@@ -28,4 +24,3 @@ export default class Board {
   }
 
 }
-
