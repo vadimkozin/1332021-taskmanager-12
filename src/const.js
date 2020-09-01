aa6e825372f63ca0e77d5387c7d66a71c2763424
Vadim Kozin <ulin.salma@yandex.ru>
2020-09-01 17:19:55 +0300

    добавил ифраструктуру фильтров: модель, презентер, обновил представл.

--- src/const.js
+++ src/const.js
@@ -1,6 +1,14 @@
-export const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
-
 export const ESCAPE_CODE = 27;
+
+export const Color = {
+  BLACK: `black`,
+  YELLOW: `yellow`,
+  BLUE: `blue`,
+  GREEN: `green`,
+  PINK: `pink`
+};
+
+export const COLORS = Object.values(Color);
 
 export const SortType = {
   DEFAULT: `default`,
@@ -8,3 +16,29 @@ export const SortType = {
   DATE_UP: `date-up`
 };
 
+export const UserAction = {
+  UPDATE_TASK: `UPDATE_TASK`,
+  ADD_TASK: `ADD_TASK`,
+  DELETE_TASK: `DELETE_TASK`
+};
+
+export const UpdateType = {
+  PATCH: `PATCH`,
+  MINOR: `MINOR`,
+  MAJOR: `MAJOR`
+};
+
+export const FilterType = {
+  ALL: `all`,
+  OVERDUE: `overdue`,
+  TODAY: `today`,
+  FAVORITES: `favorites`,
+  REPEATING: `repeating`,
+  ARCHIVE: `archive`
+};
+
+export const MenuItem = {
+  ADD_NEW_TASK: `ADD_NEW_TASK`,
+  TASKS: `TASKS`,
+  STATISTICS: `STATISTICS`
+};
